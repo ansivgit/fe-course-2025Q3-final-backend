0d82d9aa434ec99840dd30893eca45ba48d21ad8
ansivgit <ansiv@yandex.ru>
2026-03-15 21:01:18 +0300

    feat: 52 - add user controller

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,4 +1,5 @@
 export { AuthController } from './auth.controller';
 export { chatController } from './chat.controller';
 export { dictionaryController } from './dictionary.controller';
+export { UserController } from './user.controller';
 export { WidgetController } from './widget.controller';

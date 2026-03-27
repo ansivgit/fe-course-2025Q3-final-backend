e00843dd8571b5f2a914cf64b6708131fb8dcdbe
ansivgit <ansiv@yandex.ru>
2026-03-27 20:40:20 +0300

    fix: 59 - remove unused props from User

--- src/types/user.types.ts
+++ src/types/user.types.ts
@@ -5,14 +5,9 @@ export type User = {
   name: string;
   login: string;
   password: string;
-  session: Session[];
-  settings: Settings;
   createdAt: number;
   updatedAt: number;
 };
-
-type Settings = unknown;
-type Session = unknown;
 
 export type NewUser = Pick<User, 'login' | 'password' | 'name'>;
 export type LoginUser = Pick<User, 'login' | 'password'>;
